dc04f52befde02bec18f557dfddf89de4ac16666
crumbtoo <95563276+crumbtoo@users.noreply.github.com>
2022-01-01 03:19:23 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@
 #include <fstream>
 #include <math.h>
 #include <cmath>
+#include <cctype>
 
 using namespace std;
 
-int main() {
+int main(int argc, char **argv) {
 
 	// OS VAR
 	string line;
@@ -39,42 +40,101 @@ int main() {
 	}
 
 
-// final output
-// i really should have made these ansi codes into variables. whoops
-cout<<"   \e[1m\e[38;5;202m\e[1m\e[38;5;255m_,-='\e[1m\e[38;5;202m\"-.__               /\\_/\\      \n";
-cout<<"   \e[1m\e[38;5;255m`-.}       \e[1m\e[38;5;202m`=._,.-==-._.,  \033[0m0 0\e[1m\e[38;5;202m._,\n";
-cout<<"      \e[1m\e[38;5;255m`\e[1m\e[38;5;202m-.__   _,-.   )       _,.-'\n";
-cout<<"           `\"     \e[1m\e[38;5;232mG\e[1m\e[38;5;202m..\e[1m\e[38;5;232mm\e[1m\e[38;5;202m-\"^\e[1m\e[38;5;232mm\e[1m\e[38;5;202m`\e[1m\e[38;5;232mm\e[1m\e[38;5;202m'\n";
-cout<<"\033[0m\e[38;5;064m┌─────────────────────────────────────┐\n";
+	// final output
+	// i really should have made these ansi codes into variables. whoops
+	cout<<"   \e[1m\e[38;5;202m\e[1m\e[38;5;255m_,-='\e[1m\e[38;5;202m\"-.__               /\\_/\\      \n";
+	cout<<"   \e[1m\e[38;5;255m`-.}       \e[1m\e[38;5;202m`=._,.-==-._.,  \033[0m0 0\e[1m\e[38;5;202m._,\n";
+	cout<<"      \e[1m\e[38;5;255m`\e[1m\e[38;5;202m-.__   _,-.   )       _,.-'\n";
+	cout<<"           `\"     \e[1m\e[38;5;232mG\e[1m\e[38;5;202m..\e[1m\e[38;5;232mm\e[1m\e[38;5;202m-\"^\e[1m\e[38;5;232mm\e[1m\e[38;5;202m`\e[1m\e[38;5;232mm\e[1m\e[38;5;202m'\n";
+	cout<<"\033[0m\e[38;5;064m┌─────────────────────────────────────┐\e[38;5;094m\n";
 
   	// WM VAR
 	string wm = getenv("GDMSESSION");
 
 	// output vars
+	// string osF = "os: " + os;
+	// string wmF = "wm: " + wm;
+
 	string osF = "os: " + os;
 	string wmF = "wm: " + wm;
 
 	// beep boop translate that sweet sweet lua logic into this janky shit
   int x = floor(37 - osF.length() - wmF.length()); //- osF.length() - wmF.length();
   
+  string spc1;
+  string spc2;
+
   if(x % 3 == 0) {
 
     string spc1(floor(x / 3), ' ');
     string spc2(floor(x / 3), ' ');
-	cout << "\e[38;5;094m│"<< spc1 << "\e[1m\e[38;5;202m" << osF << spc2 << wmF << "\033[0m" << spc1 << "\e[38;5;094m│"; // os & wm
+    cout << "│"<< spc1 << "\e[1m\e[38;5;202m" << osF << spc2 << wmF << "\033[0m" << spc1 << "\e[38;5;094m│"; // os & wm
 
   }
   else {
 
-    string spc1(remainder(x, 3), ' ');
-    string spc2(x - remainder(x, 3), ' ');
-	cout << "│"<< spc1 << "\e[1m\e[38;5;202m" << osF << spc2 << wmF << "\033[0m" << spc1 << "│"; // os & wm
+    int rem = remainder(x, 3);
+
+    if(remainder(rem, 2) == 0) {
+      
+      string spc1(floor(x / 3) + rem / 2, ' ');
+      string spc2(floor(x / 3), ' ');
+
+      cout << "│"<< spc1 << "\e[1m\e[38;5;202m" << osF << spc2 << wmF << "\033[0m" << spc1 << "\e[38;5;094m│"; // os & wm
+
+    }
+    else {
+
+      string spc1(floor(x / 3), ' ');
+      string spc2(floor(x / 3) + rem, ' ');
+
+	  cout << "│"<< spc1 << "\e[1m\e[38;5;202m" << osF << spc2 << wmF << "\033[0m" << spc1 << "\e[38;5;094m│"; // os & wm
+
+    }
 
   }
-
 	
-cout<<"\n└─────────────────────────────────────┘\n";
+	cout<<"\n└─────────────────────────────────────┘\n";
 
+
+
+    for (int i = 0; i < argc; i++) {
+
+        string arg = argv[i];
+
+
+        if(arg == "--debug" || arg == "-d") {
+
+            cout<<"\n";
+			if(x % 3 == 0) {
+
+    			string spc1(floor(x / 3), ' ');
+  				string spc2(floor(x / 3), ' ');
+
+				cout<<"\nIF";
+				cout<<"\nx    :  "<<x;
+				cout<<"\nspc1 :  "<<spc1.length();
+				cout<<"\nspc2 :  "<<spc2.length();
+
+			}
+			else {
+
+  				string spc1(remainder(x, 3), ' ');
+  				string spc2(x - remainder(x, 3), ' ');
+
+				cout<<"\nELSE";
+				cout<<"\nx    :  "<<x;
+				cout<<"\nspc1 :  "<<spc1.length();
+				cout<<"\nspc2 :  "<<spc2.length();
+
+			}
+
+
+			cout<<"\n";
+
+        }
+
+    }
 
 	return 0;
 
